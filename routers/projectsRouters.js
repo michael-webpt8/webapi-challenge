d4357e1fe4da81195881d12c2fbecf8816cb1a2d
Michael Williams <manicdesigner@gmail.com>
2019-12-14 13:08:46 -0500

    completed update endpoint

--- routers/projectsRouters.js
+++ routers/projectsRouters.js
@@ -51,8 +51,35 @@ router.post('/', (req, res) => {
  * UPDATE
  * ENDPOINT: `/projects`
  */
-// router.put('/:id', (req, res) => {
-
-// })
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  projectDb.get(id).then(project => {
+    if (!project) {
+      return res.status(404).json({ message: 'Did not find project ID' });
+    }
+    if (!req.body.name) {
+      return res.status(400).json({ message: 'Please enter a Name.' });
+    }
+    if (!req.body.description) {
+      return res.status(400).json({ message: 'Please enter a Description.' });
+    }
+    const updateMessage = {
+      name: req.body.name,
+      description: req.body.description,
+      completed: req.body.completed || false
+    };
+    projectDb
+      .update(id, updateMessage)
+      .then(update => {
+        res.status(201).json(update);
+      })
+      .catch(err => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ errorMessage: 'Server error Updating Project Message' });
+      });
+  });
+});
 
 module.exports = router;
